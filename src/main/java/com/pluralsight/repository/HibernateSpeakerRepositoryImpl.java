0516c6e52aef942b9ef38e29513dab231629d6e8
Lan Lugar <lanlugar@gmail.com>
2023-01-20 13:53:56 +0100

    initial commit

---
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@Repository("speakerRepository")
+@Profile("dev")
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Value("#{ T(java.lang.Math).random() * 100 }")
+    private double seedNum;
+
+    @Override
+    public List<Speaker> findAll() {
+        List<Speaker> speakers = new ArrayList<Speaker>();
+
+        Speaker speaker = new Speaker();
+        speaker.setFirstName("Bryan");
+        speaker.setLastName("Hansen");
+        speaker.setSeedNum(seedNum);
+
+        speakers.add(speaker);
+        return speakers;
+    }
+}
---
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,41 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service("speakerService")
+@Profile("dev")
+public class SpeakerServiceImpl implements SpeakerService {
+
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl no args constructor");
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl repository constructor");
+        this.repository = repository;
+    }
+    @PostConstruct
+    private void initialize() {
+        System.out.println("We're called after the constructors");
+    }
+    @Override
+    public List<Speaker> findAll() {
+        return repository.findAll();
+    }
+
+    @Autowired
+    public void setRepository(SpeakerRepository repository) {
+        System.out.println("SpeakerServiceImpl setter");
+        this.repository = repository;
+    }
+
+}
